516913b94e178e7e8381a4088ae05ef1af9968f8
Tung Nguyen <tunggia2912@gmail.com>
2020-12-25 15:20:16 +0700

    fix config

---
+++ database/seeders/userSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class userSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "tung2912@gmail.com";
+        $user->password = Hash::make('123456');
+        $user->role_id = '1';
+    }
+}
